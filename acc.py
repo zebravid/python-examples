e4d710097ea19c83f5df3e833c5e028369ed18e5
zebravid <zebravid@gmail.com>
2019-04-01 01:05:39 +0300

    Class example

---
+++ acc.py
@@ -0,0 +1,11 @@
+class Acco:
+	def __init__(self,filename):
+		with open(filename,"r") as file:
+			self.balance=int(file.read())
+	
+	def withdrow(self,amount):
+		self.balance=self.balance-amount
+	def deposit(self,amount):
+		self.balance=self.balance+amount
+account=Acco("bal.txt")
+print(account.balance)
