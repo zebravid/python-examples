7b56d335973e1eedc367afff956433e57fb9d9e5
zebravid <zebravid@gmail.com>
2019-04-01 11:18:06 +0300

    Small developmen
    Example inheritance

--- acc.py
+++ acc.py
@@ -1,14 +1,25 @@
 class Acco:
 	def __init__(self,filename):
+		self.filepath=filename
 		with open(filename,"r") as file:
 			self.balance=int(file.read())
 	
 	def withdrow(self,amount):
 		self.balance=self.balance-amount
+		self.commit()
 	def deposit(self,amount):
 		self.balance=self.balance+amount
-account=Acco("bal.txt")
+		self.commit()
+	def commit(self):
+		with open(self.filepath,'w') as file:
+			file.write(str(self.balance))
+class Checking(Acco):
+	def __init__(self,filepath):
+		Acco.__init__(self,filepath)
+		
+		
+chec=Checking("bal.txt")
 
-print(account.balance)
-account.withdrow(100)
-print(account.balance)
+print(chec.balance)
+chec.withdrow(100)
+print(chec.balance)
