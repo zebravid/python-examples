7cb40d2af8705a268a6b55e80b8024efafd78b29
zebravid <zebravid@gmail.com>
2019-04-10 00:19:57 +0300

    Spelling mistakes fix

--- Qt/main.py
+++ Qt/main.py
@@ -1,5 +1,6 @@
 import sys
 import forms
+import os
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget, QLabel
@@ -13,12 +14,19 @@ class ExampleApp(QtWidgets.QMainWindow, forms.Ui_Form):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.browseFun)
     def browseFun(self):
+    	
     	self.textEdit.clear()
+    	self.textEdit.append("77777")
     	derectory=QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
-    	if directory:
-        	for filename in os.listdir(directory):
-        		textEdit.append(filename)
+    	
+    	if derectory:
+    		
+        	for filename in os.listdir(derectory):
+        		self.textEdit.append(filename)
         	
+        		
+   	
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = ExampleApp()
